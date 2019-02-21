4b8353f66d1d2016d364a6aad3ca3c88d985fbf3
johnpoint <me@lvcshu.info>
2019-02-21 16:44:32 +0800

    fix

--- web/getcerfile.php
+++ web/getcerfile.php
@@ -5,8 +5,13 @@ $secret=$_GET['secret'];
 $SQLsecret=settingsDbEdit('view','secret',NULL,NULL);
 
 if ($secret == $SQLsecret) {
-    $myfile = fopen('ssl/'.$_GET['file'], "r") or die("Unable to open file!");
-    echo fread($myfile, filesize('ssl/'.$_GET['file']));
+    if ($_GET['fulldir'] == '' ) {
+        $dir=$_GET['file'];
+    } else {
+        $dir=$_GET['fulldir'];
+    }
+    $myfile = fopen('ssl/'.$dir, "r") or die("Unable to open file!");
+    echo fread($myfile, filesize('ssl/'.$dir));
     fclose($myfile);
 } else {
     echo '{"code":"1","error_text":"SECRET error"}';
