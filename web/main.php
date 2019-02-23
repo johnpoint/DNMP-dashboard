7857fb88184c6a3e17708ceb4e6239753b36ee60
johnpoint <me@lvcshu.info>
2019-02-23 20:06:06 +0800

    update

--- web/main.php
+++ web/main.php
@@ -5,7 +5,11 @@ echo '  <div class="mdui-container doc-container doc-no-cover">
 <h1 class="doc-title mdui-text-color-theme">概况</h1>
 <div class="doc-chapter">
     <div class="mdui-typo">
-        <h2>目前注册服务器数目:</h2><p>'.$info['num'].'</p>
+        <div class="mdui-panel-item-header mdui-panel-item-open">服务器</div>
+        <div class="mdui-panel-item-body">
+            <code>'.$info['num'].'</code>
+        </div>
+    </div>
     </div>
 </div>
   ';
