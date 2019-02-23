0abf5a7528f8bd9d03f52fbc32c33105820d0bb9
johnpoint <me@lvcshu.info>
2019-02-23 20:50:50 +0800

    add about

--- web/about.php
+++ web/about.php
@@ -8,6 +8,8 @@ echo '<div class="mdui-container doc-container doc-no-cover">
     <div class="mdui-typo">
     <h1>开发者</h1>
     <li><a href="https://github.com/johnpoint">johnpoint</a></li>
+    <h1>API</h1>
+    <li><a href="https://ip.sb">IP.SB</a></li>
     <h1>THANKS</h1>
     <li><a href="https://github.com/BotoX/ServerStatus">BotoX/ServerStatus</a></li>
     <li><a href="https://github.com/zdhxiong/mdui">zdhxiong/mdui</a></li>
