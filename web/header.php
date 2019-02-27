cf6150a991aa04b3554547444461e008e6bdd4c6
johnpoint <jahanngauss414@gmail.com>
2019-02-27 14:53:24 +0800

    fix

--- web/header.php
+++ web/header.php
@@ -2,19 +2,15 @@
 <header class="mdui-appbar mdui-appbar-fixed">
     <div class="mdui-toolbar mdui-color-theme">
         <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white"
-              mdui-drawer="{target: \'#main-drawer\', swipe: true}"><i class="mdui-icon material-icons">menu</i></span>
+              mdui-drawer="{target: '#main-drawer', swipe: true}"><i class="mdui-icon material-icons">menu</i></span>
         <a href="../" class="mdui-typo-headline mdui-hidden-xs">服务器</a>
         <a href="./" class="mdui-typo-title">管理</a>
-        <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: \'夜间模式\'}">
+        <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: '夜间模式'}">
             <i id="dark-day" class="mdui-icon material-icons">color_lens</i></span>
     </div>
     <script>
         $('#dark-day').click(function () {
-            if ($(\
-            '.mdui-theme-layout-dark'
-        ).
-            length == 0
-        )
+            if ($('.mdui-theme-layout-dark').length == 0)
             {
                 $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink"
             }
@@ -22,6 +18,6 @@
             {
                 $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-theme-layout-dark mdui-loaded"
             }
-        })
+        });
     </script>
 </header>
