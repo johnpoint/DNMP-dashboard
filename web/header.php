5639510f2eefe606be627f2e006d234836c6d09d
johnpoint <me@lvcshu.info>
2019-02-28 17:51:44 +0800

    fix

--- web/header.php
+++ web/header.php
@@ -14,12 +14,10 @@ echo '<body class="mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-pri
         $('#dark-day').click(function () {
             if ($('.mdui-theme-layout-dark').length == 0) {
                 $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-theme-layout-dark mdui-loaded";
-                setActiveStyleSheet('dark');
                 document.cookie = "darkday=mdui-theme-layout-dark";
                 document.cookie = "style=dark";
             } else {
                 $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-loaded";
-                setActiveStyleSheet('light');
                 document.cookie = "darkday=null";
                 document.cookie = "style=light";
             }
