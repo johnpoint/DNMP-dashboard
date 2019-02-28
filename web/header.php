156a26d76830e50ce15cb15868fba045e4fe71d6
johnpoint <me@lvcshu.info>
2019-02-28 23:19:06 +0800

    update
    + js脚本迁移至 footer.php

--- web/header.php
+++ web/header.php
@@ -10,17 +10,4 @@ echo '<body class="mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-pri
         <span class="mdui-btn mdui-btn-icon mdui-ripple mdui-ripple-white" mdui-tooltip="{content: '夜/日模式'}">
             <i id="dark-day" class="mdui-icon material-icons">color_lens</i></span>
     </div>
-    <script>
-        $('#dark-day').click(function () {
-            if ($('.mdui-theme-layout-dark').length == 0) {
-                $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-theme-layout-dark mdui-loaded";
-                document.cookie = "darkday=mdui-theme-layout-dark";
-                document.cookie = "style=dark";
-            } else {
-                $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-loaded";
-                document.cookie = "darkday=null";
-                document.cookie = "style=light";
-            }
-        });
-    </script>
 </header>
