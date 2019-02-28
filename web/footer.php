3d33909eff704f256cde9779ddb543fb9752cb97
johnpoint <me@lvcshu.info>
2019-02-28 23:26:16 +0800

    fix

--- web/footer.php
+++ web/footer.php
@@ -4,12 +4,12 @@
     $('#dark-day').click(function () {
         if ($('.mdui-theme-layout-dark').length == 0) {
             $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-theme-layout-dark mdui-loaded";
-            setActiveStyleSheet('drak')
+            setActiveStyleSheet('drak');
             document.cookie = "darkday=mdui-theme-layout-dark";
             document.cookie = "style=dark";
         } else {
             $('body')["0"].className = "mdui-drawer-body-left mdui-appbar-with-toolbar mdui-theme-primary-indigo mdui-theme-accent-pink mdui-loaded";
-            setActiveStyleSheet('light')
+            setActiveStyleSheet('light');
             document.cookie = "darkday=null";
             document.cookie = "style=light";
         }
