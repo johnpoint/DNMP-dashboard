30c4aef92e5ee83295664450d642dfdab6ac48d9
johnpoint <me@lvcshu.com>
2019-04-06 10:06:49 +0800

    update

---
+++ web/js/updateStatus.js
@@ -0,0 +1,112 @@
+$('td.service').click(function () {
+    hitpoint = this;
+    if (this.bgColor == 'green') {
+        $.ajax({
+            url: 'api.php',
+            method: 'GET',
+            data: { do: 'repo', ipv4: this.attributes['4'].nodeValue, key: this.attributes['0'].nodeValue, value: '10', secret: document.getElementById('nonediv').innerHTML },
+            success: function () {
+                hitpoint.bgColor = 'yellow';
+                hitpoint.innerHTML = 'close';
+            }
+        });
+    } else {
+        $.ajax({
+            url: 'api.php',
+            method: 'GET',
+            data: { do: 'repo', ipv4: this.attributes['4'].nodeValue, key: this.attributes['0'].nodeValue, value: '11', secret: document.getElementById('nonediv').innerHTML },
+            success: function () {
+                hitpoint.bgColor = 'yellow';
+                hitpoint.innerHTML = 'opening';
+            }
+        });
+    }
+});
+$('#update').click(function () {
+    window.location.reload();
+});
+var msg = 0;
+function checkUpdate() {
+    $.ajax({
+        url: 'api.php',
+        method: 'GET',
+        data: { do: 'checkupdate', secret: $('#secret')[0].innerText },
+        success: function (data) {
+            msg = data;
+            a = JSON.parse(msg);
+            for (var i = 0; i < a['info']['length']; i++) {
+                for (var j = 4; j < 8; j++) {
+                    switch (j) {
+                        case 4:
+                            if (a['info'][i]['nginx'] == 1) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'running';
+                                $('#' + i + '' + j)[0].bgColor = 'green';
+                            } else if (a['info'][i]['nginx'] == 0) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'deaded';
+                                $('#' + i + '' + j)[0].bgColor = 'red';
+                            } else if (a['info'][i]['nginx'] == 10) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'close';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            } else if (a['info'][i]['nginx'] == 11) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'opening';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            }
+                            break;
+                        case 5:
+                            if (a['info'][i]['phpfpm'] == 1) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'running';
+                                $('#' + i + '' + j)[0].bgColor = 'green';
+                            } else if (a['info'][i]['phpfpm'] == 0) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'deaded';
+                                $('#' + i + '' + j)[0].bgColor = 'red';
+                            } else if (a['info'][i]['phpfpm'] == 10) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'close';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            } else if (a['info'][i]['phpfpm'] == 11) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'opening';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            }
+                            break;
+                        case 6:
+                            if (a['info'][i]['mysql'] == 1) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'running';
+                                $('#' + i + '' + j)[0].bgColor = 'green';
+                            } else if (a['info'][i]['mysql'] == 0) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'deaded';
+                                $('#' + i + '' + j)[0].bgColor = 'red';
+                            } else if (a['info'][i]['mysql'] == 10) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'close';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            } else if (a['info'][i]['mysql'] == 11) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'opening';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            }
+                            break;
+                        case 7:
+                            if (a['info'][i]['proxy'] == 1) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'running';
+                                $('#' + i + '' + j)[0].bgColor = 'green';
+                            } else if (a['info'][i]['proxy'] == 0) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'deaded';
+                                $('#' + i + '' + j)[0].bgColor = 'red';
+                            } else if (a['info'][i]['proxy'] == 10) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'close';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            } else if (a['info'][i]['proxy'] == 11) {
+                                $('#' + i + '' + j)[0]['innerText'] = 'opening';
+                                $('#' + i + '' + j)[0].bgColor = 'yellow';
+                            }
+                            break;
+                    }
+
+                }
+            }
+        }
+    });
+};
+
+const timeId = setInterval(() => {
+    if (true) {
+        clearInterval(this.timeId)
+    }; checkUpdate();
+}, 3000)
