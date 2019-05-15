f2174826585e1765ca7da2f0b2e3f8a1cb976430
johnpoint <me@lvcshu.com>
2019-05-15 21:10:51 +0800

    update

--- web/head.php
+++ web/head.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp">
-    <link rel="stylesheet" href="//cdn.lvcshu.info/mdui/css/mdui.min.css">
-    <script src="//cdn.lvcshu.info/mdui/js/mdui.min.js"></script>
+    <link href="https://cdn.bootcss.com/mdui/0.4.2/css/mdui.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/mdui/0.4.2/js/mdui.min.js"></script>
     <script src="//cdn.lvcshu.info/js/anti-360browser.js"></script>
     <script
             src="//cdn.lvcshu.info/js/jq/jquery-3.3.1.min.js"
