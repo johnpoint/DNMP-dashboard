93734daf90db1ca7908998303f56bb68c4d90928
johnpoint <me@lvcshu.com>
2019-05-15 20:56:27 +0800

    update

--- web/head.php
+++ web/head.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/css/mdui.min.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/js/mdui.min.js"></script>
+    <link rel="stylesheet" href="//cdn.lvcshu.info/css/mdui/mdui.min.css">
+    <script src="//cdn.lvcshu.info/js/mdui/mdui.min.js"></script>
     <script src="//cdn.lvcshu.info/js/anti-360browser.js"></script>
     <script
-            src="//code.jquery.com/jquery-3.3.1.min.js"
+            src="//cdn.lvcshu.info/js/jq/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"></script>
     <title>服务器管理</title>
